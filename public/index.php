87d9efbacec9721212336d15f03a6e2d8846b224
Luiz Guilherme V Nunes <luizguilherme.cg@gmail.com>
2020-11-30 20:42:41 -0300

    Iniciando o Padrão MVC no Projeto

--- public/index.php
+++ public/index.php
@@ -1,18 +1,7 @@
 <?php
 
+require_once '../app/init.php';
 
-echo "<p><h>Testando Virtual Directories no GearHost !!</h></p>";
+$app = new App;
 
-phpinfo();
-
-$pdo = null;
-
-try {
-    $pdo = new PDO('mysql:host=mysql;dbname=database', 'root', '6543');
-} catch (PDOException $e) {
-    print $e->getMessage();
-    die();
-}
-
-var_dump($pdo);
-
+?>
