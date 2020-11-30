31e3405582fecd85eaabd40c88c9690d635c8407
Luiz Guilherme V Nunes <luizguilherme.cg@gmail.com>
2020-11-30 16:08:10 -0300

    Envio n. 2 para testar o deploy do Cloud

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
 
+/*
 echo "Testando Virtual Directories no GearHost !!";
+*/
+
+$texto = "eu não sou besta pra tirar onda de herói";
+
+echo substr($texto, 0, 16);  // eu não sou besta
+echo "<br>\\n";
+echo substr($texto, 11);     // besta pra tirar onda de herói
+echo "<br>\\n";
+echo substr($texto, 11, 9);  // besta pra
+echo "<br>\\n";
+echo substr($texto, -5);     // herói
+echo "<br>\\n";
 
 phpinfo();
 
@@ -14,3 +27,4 @@ try {
 }
 
 var_dump($pdo);
+
